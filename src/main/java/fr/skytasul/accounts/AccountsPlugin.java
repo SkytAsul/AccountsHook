f52f56198644d571b8d2fa8ff3fad0f2b623d983
SkytAsul <skytasul@gmail.com>
2025-11-26 10:30:47 +0100

    🔊 Added logging for registered account providers

--- src/main/java/fr/skytasul/accounts/AccountsPlugin.java
+++ src/main/java/fr/skytasul/accounts/AccountsPlugin.java
@@ -1,24 +1,35 @@
 package fr.skytasul.accounts;
 
 import fr.skytasul.accounts.uuid.UUIDAccountsProvider;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServiceRegisterEvent;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class AccountsPlugin extends JavaPlugin {
+public class AccountsPlugin extends JavaPlugin implements Listener {
 
 	private UUIDAccountsProvider uuidProvider = new UUIDAccountsProvider();
 
 	@Override
 	public void onEnable(){
+		getServer().getPluginManager().registerEvents(this, this);
+
 		getServer().getPluginManager().registerEvents(uuidProvider, this);
 		getServer().getServicesManager().register(AccountsProvider.class, uuidProvider, this, ServicePriority.Lowest);
-
-		getLogger().info("Registered default accounts provider: uuid");
 	}
 
 	@Override
 	public void onDisable(){
 		getServer().getServicesManager().unregister(uuidProvider);
+	}
+
+	@EventHandler
+	public void onServiceRegistered(ServiceRegisterEvent event) {
+		if (event.getProvider().getProvider() instanceof AccountsProvider accountsProvider) {
+			getLogger().info("Registered accounts provider %s with priority %s".formatted(accountsProvider.getName(),
+					event.getProvider().getPriority().name()));
+		}
 	}
 
 }
